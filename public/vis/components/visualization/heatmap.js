37c863884b4c4e694dffd86df603e5aad89f136b
Shelby Sturgis <shelbyjsturgis@gmail.com>
2016-03-16 02:33:50 -0700

    Closes #16. Adds upperlimit to legend

--- public/vis/components/visualization/heatmap.js
+++ public/vis/components/visualization/heatmap.js
@@ -153,7 +153,7 @@ function heatmap() {
 
       // Legend
       container
-        .datum([0].concat(colorScale.quantiles()))
+        .datum([0].concat(colorScale.quantiles()).concat(colorScale.domain()[1]))
         .call(legend);
     });
   }
